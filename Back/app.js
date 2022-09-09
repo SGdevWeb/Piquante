33902214a68344d4489aa52904b27e63651514bd
SGdevWeb <samuel.gustin.dev@gmail.com>
2022-09-09 20:57:25 +0200

    Création du parcours utilisateur avec connexion et authentification

---
+++ Back/app.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config({ path: './config.env' });
+const dataBase = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(dataBase,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(express.json());
+
+// app.use((req, res, next) => {
+//     res.json({ message: 'Votre requête a bien été reçue !' });
+//     next();
+// });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
--- Back/server.js
+++ Back/server.js
@@ -1,9 +1,11 @@
+
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end(' Requête envoyée !')
-});
 
-server.listen(3000, () => {
-    console.log(' listening on port 3000 ...')
+app.set('port', process.env.PORT || 3000)
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('listening ...')
 });
