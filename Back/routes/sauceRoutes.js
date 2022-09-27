9993bbca178b099b9064ba941e00d6d15ae661bd
SGdevWeb <samuel.gustin.dev@gmail.com>
2022-09-27 16:29:13 +0200

    Ajout de l'authentification sur toutes les routes Sauces

--- Back/routes/sauceRoutes.js
+++ Back/routes/sauceRoutes.js
@@ -6,10 +6,10 @@ const multer = require('../middleware/multer-config');
 const sauceController = require('../controllers/sauceController');
 
 router.post('/', auth, multer, sauceController.createSauce);
-router.get('/:id', sauceController.GetOneSauce);
-router.get('/', sauceController.getAllSauces);
+router.get('/:id', auth, sauceController.GetOneSauce);
+router.get('/', auth, sauceController.getAllSauces);
 router.put('/:id', auth, multer, sauceController.modifySauce);
 router.delete('/:id', auth, sauceController.deleteSauce);
-router.post('/:id/like', sauceController.likeSauce);
+router.post('/:id/like', auth, sauceController.likeSauce);
 
 module.exports = router;
